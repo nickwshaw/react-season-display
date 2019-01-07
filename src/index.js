9cb7d14fe49a97b4f00cff05ee841d9991b5cd35
Nicholas Shaw <Nicholas.Shaw@dixsonscarphone.com>
2019-01-07 11:17:54 +0000

    Removed boilerplate and added season display

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import SeasonDisplay from './SeasonDisplay';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { latitude : null, errorMessage : '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log(position);
+                this.setState({ latitude : position.coords.latitude })
+            },
+            (error) => this.setState({ errorMessage : error.message })
+        );
+    }
+
+    render() {
+        if (!this.state.latitude && this.state.errorMessage) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (this.state.latitude && !this.state.errorMessage) {
+            return <div>Lattitude: {this.state.latitude}</div>;
+        }
+
+        return <div>Loading...</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
